a38d38816bff9998e5ebacf60dc0669ad7c43ad8
Winston <wagawc@gmail.com>
2019-02-04 15:13:54 +0300

    add syntax inside route decorator to support urls with dynamic section in them

--- app/views.py
+++ app/views.py
@@ -4,6 +4,16 @@ from app import app
 @app.route('/')
 def index():
     """
-    view root page function that returns the index p0age and its data
+    view root page function that returns the index page and its data
     """
-    return render_template('index.html')
+    message = 'Wossup MovieHolics'
+    
+    return render_template('index.html',message = message)
+
+@app.route('/news/<int:news_id>')    
+def news(news_id)    
+    """
+    view news function that returns the movie details page and its data
+    """
+    return render_template('news.html',id = news_id)
+    
