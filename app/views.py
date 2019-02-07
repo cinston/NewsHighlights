48ccef8efd93637a2a35c8af417c6a3a3c67ae6d
Winston <wagawc@gmail.com>
2019-02-07 12:09:22 +0300

    import get_movies function from request module to help us get popular news from our api then display the popular news to our template

--- app/views.py
+++ app/views.py
@@ -1,11 +1,15 @@
 from flask import render_template
 from app import app
+from .request import get_news
 
 @app.route('/')
 def index():
     """
     view root page function that returns the index page and its data
     """
+    popular_news = get_news('popular')
+    print(popular_news)
+    
     title = 'Home - This is a news website that lists and previews news articles from various sources.'    
     return render_template('index.html', title = title)
 
