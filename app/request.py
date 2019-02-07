97fd7f553a1137cffd420fcd547e911388ce6fb1
Winston <wagawc@gmail.com>
2019-02-07 11:12:25 +0300

    create request.py file where we write code to make requests to our api

--- app/request.py
+++ app/request.py
@@ -1,11 +1,11 @@
-# from app import app
+from app import app
 # import urllib.request,json
 # from .models import news
 
 
 # News = news.News
 
-# api_key = app.config['NEWS_API_KEY']
+api_key = app.config['NEWS_API_KEY']
 
 # base_url = app.config["NEWS_API_BASE_URL"]
 
