8ebf5490aa8abcb1e51d59b90741196956b9e7b4
Winston <wagawc@gmail.com>
2019-02-07 11:25:09 +0300

    corrected config.py

--- app/config.py
+++ app/config.py
@@ -5,24 +5,24 @@ class Config:
 
     NEWS_API_BASE_URL = 'https://newsapi.org/v2/sources?apiKey={}'
 
-# class ProdConfig(Config):
-#     """
-#     Production configuration child class
+class ProdConfig(Config):
+    """
+    Production configuration child class
 
-#     Args:
-#         Config: The parent configuration class with General configuration settings
+    Args:
+        Config: The parent configuration class with General configuration settings
 
-#     """
+    """
 
-#     pass
+    pass
 
-# class DevConfig(Config):
-#     """
-#     Development configuration child class
+class DevConfig(Config):
+    """
+    Development configuration child class
 
-#     Args:
-#         Config: The parent configuration class with General configuration settings
+    Args:
+        Config: The parent configuration class with General configuration settings
 
-#     """
+    """
 
-#     DEBUG = True
+    DEBUG = True
