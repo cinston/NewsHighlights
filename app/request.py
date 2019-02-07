43feb2c8d707060710deda41861b09b5780f7b57
Winston <wagawc@gmail.com>
2019-02-07 12:00:17 +0300

    create function that processes the results and create news objects from elements we need

--- app/request.py
+++ app/request.py
@@ -24,4 +24,28 @@ def get_news(category):
         if get_news_response['results']:
             news_results_list = get_news_response['results']
             news_results = process_results(news_results)
+def procee_results(news_list):
+    """
+    Function  that processes the news result and transform them to a list of Objects
+
+    Args:
+        news_list: A list of dictionaries that contain news details
+
+    Returns :
+        news_results: A list of news objects
+    """    
+    
+    news_results = []
+    for news_item in news_list:
+        author = news_item.get(author)
+        title = news_item.get(title)
+        description = news_item.get(description)
+        url = news_item.get(url)
+        urlToImage = news_item.get(urlToImage)
+        publishedAt = news_item.get(publishedAt)
+        content = news_item.get(content)
+
+        if poster:
+            news_object = News(author,title,description,url,urlToImage,publishedAt,content)
+            news_results.append(news_object)
     return news_results        
