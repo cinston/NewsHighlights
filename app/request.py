a210dba8f9ab9f04271f7c42a0d4ba4b62d30d99
Winston <wagawc@gmail.com>
2019-02-07 11:20:28 +0300

    add code to help us get data from our api

--- app/request.py
+++ app/request.py
@@ -1,9 +1,9 @@
 from app import app
-# import urllib.request,json
-# from .models import news
+import urllib.request,json
+from .models import news
 
 
-# News = news.News
+News = news.News
 
 api_key = app.config['NEWS_API_KEY']
 
